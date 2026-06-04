a5cf98bfb0262a59c5539669ba4de339e067fce0
Rajeev Kumar <pkrajgangwar@gmail.com>
2026-06-04 17:51:14 +0530

    Start

--- lib/app/startup/app.dart
+++ lib/app/startup/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:interview_preperation_buddy/app/startup/splash/splash_screens.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:interview_preperation_buddy/app/startup/app.dart';
 
 void main() {
   runApp(const MyApp());
